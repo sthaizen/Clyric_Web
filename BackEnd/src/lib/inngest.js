a40bbe07bd81c7015ea7a6d261548780b6a7f1d9
sthaizen <lalitstha33@gmail.com>
2026-03-06 20:36:41 +0545

    bug fixes

--- BackEnd/src/lib/inngest.js
+++ BackEnd/src/lib/inngest.js
@@ -3,57 +3,49 @@ import { connectDB } from "./db.js"
 import User from "../models/User.js"
 import { upsertStreamUser, deleteStreamUser } from "./streamTemp.js";
 
-
-
 export const inngest = new Inngest({ id: "Clyric" });
 
-
 const syncUser = inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
+    { id: "sync-user" },
+    { event: "clerk/user.created" },
 
-    async ({event}) =>{
+    async ({ event }) => {
         await connectDB()
 
-        const {id,email_address, first_name, last_name, image_url} = event.data
+        const { id, email_addresses, first_name, last_name, image_url } = event.data
 
-        const newUser ={
+        const newUser = {
             clerkId: id,
-            email:email_address[0]?.email_address,
+            email: email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""}`,
-            profileImage:image_url
+            profileImage: image_url
         }
+
         await User.create(newUser)
-        
 
-       await upsertStreamUser({
-        id: newUser.clerkId.tostring(),
-        name: newUser.name,
-        image: newUser.profileImage,
-       })
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.profileImage,
+        })
 
-       // send welcome email to the user
-       
+        // send welcome email to the user
     }
-
-
 )
 
 const deleteUserFromDb = inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.deleted"},
+    { id: "delete-user-from-db" },
+    { event: "clerk/user.deleted" },
 
-    async ({event}) =>{
+    async ({ event }) => {
         await connectDB()
 
-        const {id} = event.data
+        const { id } = event.data
 
-        await User.deleteOne({clerkId: id});
+        await User.deleteOne({ clerkId: id })
 
-        await deleteStreamUser(id.tostring());
+        await deleteStreamUser(id.toString())
     }
-
-
 )
 
-export const functions = [syncUser, deleteUserFromDb];
+export const functions = [syncUser, deleteUserFromDb]
